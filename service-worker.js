ae6290f461156847837b75e81d9671c153c1909d
Sajjad Hosen <145913090+S4jj4d404@users.noreply.github.com>
2025-02-28 20:44:15 +0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/icon.png',
+        // অন্যান্য প্রয়োজনীয় ফাইল যোগ করো
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
